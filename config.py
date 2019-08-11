e499be498011dbf75473bac8b5b57845a56f533c
Alberto Ivo Vieira <albertoivo@gmail.com>
2019-08-10 21:09:40 -0300

    random secret key

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-# Grabs the folder where the script runs.
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -12,10 +12,10 @@ class Config(object):
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = 'any string works here'
+    SECRET_KEY = os.urandom(32)
 
     SESSION_COOKIE_SECURE = True
 
-    SESSION_COOKIE_NAME = 'YourAppName-WebSession'
+    SESSION_COOKIE_NAME = 'Fyyur-WebSession'
 
     WTF_CSRF_TIME_LIMIT = None
